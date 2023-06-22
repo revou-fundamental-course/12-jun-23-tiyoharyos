10f73a10b21ab0521a83054fad7d7e95ca146c7c
Tiyo Haryo Subaktiono <87368516+tiyoharyos@users.noreply.github.com>
2023-06-22 08:13:21 +0700

    Update scripts.js

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -1,18 +1,13 @@
 function celToFa() {
     var celsiusInput = document.getElementById("celsiusInput").value;
   
-    // Validasi input
-    if (isNaN(celsiusInput)) {
-      alert("Masukkan suhu dalam angka.");
-      return;
-    }
-  
-    var celsius = parseFloat(celsiusInput);
-    var fahrenheit = (celsius * 9/5) + 32;
+    
+    var celsius = parseFloat(celsiusInput);//Inputam User
+    var fahrenheit = (celsius * 9/5) + 32; //rumus celcius ke farenheith
     var proses = "(" + celsius + "\u00B0 Celcius * (9 / 5)) + 32 = " + fahrenheit + "\u00B0 Fahrenheit";
   
     document.getElementById("hasil").innerHTML = fahrenheit.toFixed(2) + "&deg;F";
-    document.getElementById("explanation").innerHTML = celsius.toFixed(2) + "&deg; C sama dengan " + fahrenheit.toFixed(2) + "&deg;F.";
+    document.getElementById("explanation").innerHTML = celsius.toFixed(2) + "&deg;C sama dengan " + fahrenheit.toFixed(2) + "&deg;F.";
     document.getElementById("proses").innerText = proses;
     
   }
@@ -32,7 +27,7 @@ function celToFa() {
       var proses = "(" + fahrenheit + "\u00B0 Fahrenheit * (9 / 5)) + 32 = " + celsius + "\u00B0 Celcius ";
   
       document.getElementById("hasil").innerHTML = celsius.toFixed(2) + "&deg;C";
-      document.getElementById("explanation").innerHTML = fahrenheit.toFixed(2) + "&deg;F sama dengan " + celsius.toFixed(2) + "&deg; C.";
+      document.getElementById("explanation").innerHTML = fahrenheit.toFixed(2) + "&deg;F sama dengan " + celsius.toFixed(2) + "&deg;C.";
       document.getElementById("proses").innerText = proses;
     }
   }
